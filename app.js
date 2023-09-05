22493fa4235f8fc056d0c4028c84da9db18340d8
NatYarm <yarmnat7@gmail.com>
2023-09-05 08:43:26 +0300

    added .env

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import router from './routes/drinksRoutes.js';
 
 const app = express();
-const port = 3000;
+const dotenv = dotenv.config();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
@@ -13,6 +15,6 @@ app.use('/ingredient/:ingrName', router);
 app.use('/findCocktail', router);
 app.use('/browse/:letter', router);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
